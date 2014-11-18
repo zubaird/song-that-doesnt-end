b734a0c7bae87388e8c19a64b4cd88a025ab2483
zubaird <zubair.desai@gmail.com>
2014-11-18 14:04:36 -0800

    Completed Exercise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,8 @@
 class PagesController < ApplicationController
 
+
+  # def verse_2
+  #
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+
+  get "verse-2" => "pages#verse_2"
+  get "verse-3" => "pages#verse_3"
+  get "verse-4" => "pages#verse_4"
+
+
+
 end
